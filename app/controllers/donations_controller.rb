5e8e251f7c32d1d4af604cb902f4f67d19f9a012
Najihah Talib <najihahtalib@gmail.com>
2019-12-03 15:45:42 +0800

    controllers

---
+++ app/controllers/donations_controller.rb
@@ -0,0 +1,16 @@
+class DonationsController < ApplicationController
+    def index
+    @donations = current_user.donor.donations
+  end
+
+  def create
+    @food = Food.find(params[:food_id])
+    @donation = Donation.new
+    @donation.food = @donation
+    @donation.user = current_user
+    if @donation.save
+      redirect_to food_donation_path(@food, @donation)
+    end
+  end
+end
+end
