5bb4a9abe68c365a9bd8a589b4d25dd88be22a13
michael ng <mngksoon@gmail.com>
2019-12-03 15:13:32 +0800

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,47 +29,86 @@ User.create(
 
 puts "create donor profile for first user"
 Donor.create(
-  full_name: 'Mandy',
-  username: 'Tan',
+  full_name: 'Mandy Tan',
+  username: 'Mandy90',
   bio: 'ipsem lorem',
-  pickup_time: '6 pm',
+  pickup_time: 'Collection after 6pm',
   user: User.first
 )
+
+
+puts 'create categories'
+Category.create!(
+  name: 'Fruits'
+)
+
+
+puts 'create categories'
+Category.create!(
+  name: 'Dairy'
+)
+
+
+puts 'create categories'
+Category.create!(
+  name: 'Grains'
+)
+
+
+puts 'create categories'
+Category.create!(
+  name: 'Meats'
+)
+
+
+puts 'create categories'
+Category.create!(
+  name: 'Vegetables'
+)
+
+puts 'create categories'
+Category.create!(
+  name: 'Canned foods'
+)
+
 
 puts 'create foods'
 Food.create!(
   name: "Ripe Banana (4 large ones)",
   photo: "",
-  description: "Bought too much for my family, would be good for baking.",
-  purchase_date: "18-08-2018",
+  description: "Will be good for baking!",
+  purchase_date: Date.new(2018,8,18)
   location: "360, #08-02 Orchard Rd, International Building, 238869",
   latitude: "33.976059",
   longtitude: "-116.955040",
-  donor: Donor.first
+  donor: Donor.first,
+  category: Fruits
 )
+
 
 puts 'create foods'
 Food.create!(
   name: "Carton of eggs",
   photo: "",
-  description: "Baked too much cake. Had extras eggs.",
-  purchase_date: "21-08-2018",
+  description: "Baked too much cake. Extra eggs to give away.",
+  purchase_date: Date.new(2018,8,21),
   location: "14 Scotts Rd, Singapore 228213",
   latitude: "1.307180",
   longtitude: "103.833809",
-  donor: Donor.first
+  donor: Donor.first,
+  category: Dairy
 )
+
 
 puts 'create foods'
 Food.create!(
   name: "Heinz Tomato Ketchup",
   photo: "",
   description: "I dont like ketchup. Giving away.",
-  purchase_date: "21-08-2018",
+  purchase_date: Date.new(2018,8,30),
   location: "313 Orchard Rd, Singapore 238895",
   latitude: "1.301040",
   longtitude: "103.838379",
-  donor: Donor.first
+  donor: Donor.first,
+  category: Canned foods
 )
-
-
