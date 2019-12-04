bbd0f4c4b6169518fd73e26994274c151685fa32
Charmaine Lee <charmaine@seeksophie.com>
2019-12-04 17:20:55 +0800

    added preferred pick up days and time to donors table

--- db/migrate/20191204091612_add_pickup_days_to_donors.rb
+++ db/migrate/20191204091612_add_pickup_days_to_donors.rb
@@ -2,5 +2,4 @@ class AddPickupDaysToDonors < ActiveRecord::Migration[5.2]
   def change
     add_column :donors, :pickup_days, :string
   end
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_091454) do
+ActiveRecord::Schema.define(version: 2019_12_04_091839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 2019_12_04_091454) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "pickup_days"
+    t.time "pickup_start_time"
+    t.time "pickup_end_time"
     t.index ["user_id"], name: "index_donors_on_user_id"
   end
 
