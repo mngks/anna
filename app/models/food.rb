1300b40d26a8f292dc82905d0f70904474a25fd8
michael ng <mngksoon@gmail.com>
2019-12-04 11:50:15 +0800

    standarding seeds

--- app/models/food.rb
+++ app/models/food.rb
@@ -6,4 +6,6 @@ class Food < ApplicationRecord
   has_many :food_categories, dependent: :destroy
   has_many :categories, through: :food_categories
   has_many :donations, dependent: :destroy
+  
+  mount_uploader :photo, PhotoUploader
 end
