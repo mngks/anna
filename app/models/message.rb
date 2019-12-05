4700c79cebd91fcf84b54f5695333bab93393e61
michael ng <mngksoon@gmail.com>
2019-12-05 18:45:41 +0800

    search bar

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :donation
   belongs_to :user
+  validates_presence_of :body, :donation_id, :user_id
+
+  def message_time
+    created_at.strftime("%m/%d/%y at %l:%M %p")
+  end
 end
