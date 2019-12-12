49c73b5daca0076fd6c0c5039c188e5cb5aeac33
Najihah Talib <najihahtalib@gmail.com>
2019-12-12 20:18:49 +0800

    added validations for Food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -7,4 +7,10 @@ class Food < ApplicationRecord
   has_many :categories, through: :food_categories
   has_many :donations, dependent: :destroy
   mount_uploader :photo, PhotoUploader
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :purchase_date, presence: true
+  validates :location, presence: true
+  validates :photo, presence: true
 end
