0fb0139fc19c60412d47218f98d976630788013b
Najihah Talib <najihahtalib@gmail.com>
2019-12-12 16:07:23 +0800

    update-seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,11 +15,11 @@ puts "Creating the seeds"
 
 puts 'create users'
 User.create(
-  email: 'user1@gmail.com',
+  email: 'charmaine@gmail.com',
   password: 'user123',
   password_confirmation: 'user123',
-  username: 'Mandy',
-  remote_photo_url: "https://images.unsplash.com/photo-1525450824786-227cbef70703?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=1600",
+  username: 'Charmaine',
+  remote_photo_url: "https://res.cloudinary.com/df8xepdmu/image/upload/v1576118625/IMG_9658_d4eefw.jpg",
   bio: 'I love sharing food as the best way to anyone’s heart is through their stomach (;',
   preferred_start_time: '6pm',
   preferred_end_time: '8pm',
@@ -27,176 +27,201 @@ User.create(
 
 
 User.create(
-  email: 'user2@gmail.com',
+  email: 'mike@gmail.com',
   password: 'user123',
   password_confirmation: 'user123',
-  username: 'user2',
-  remote_photo_url: "https://images.unsplash.com/photo-1532074205216-d0e1f4b87368?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1581&q=80"
+  username: 'Mike',
+  remote_photo_url: "https://res.cloudinary.com/df8xepdmu/image/upload/v1576118641/Screenshot_20191211-192104_Tinder-01_qg7ghb.jpg"
 )
 
 User.create(
-  email: 'user3@gmail.com',
+  email: 'mandy123@gmail.com',
   password: 'user123',
   password_confirmation: 'user123',
-  username: 'user3',
-  remote_photo_url: "https://images.unsplash.com/photo-1532074205216-d0e1f4b87368?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1581&q=80"
+  username: 'Mandy',
+  remote_photo_url: "https://images.unsplash.com/photo-1562003389-902303a38425?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1429&q=80"
 )
 
 User.create(
-  email: 'user4@gmail.com',
+  email: 'anna123@gmail.com',
   password: 'user123',
   password_confirmation: 'user123',
-  username: 'user4',
-  remote_photo_url: "https://images.unsplash.com/photo-1532074205216-d0e1f4b87368?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1581&q=80"
+  username: 'Anna',
+  remote_photo_url: "https://images.unsplash.com/photo-1548958921-c5c0fe1b307d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+)
+
+User.create(
+  email: 'george123@gmail.com',
+  password: 'user123',
+  password_confirmation: 'user123',
+  username: 'George',
+  remote_photo_url: "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
 )
 
 
 puts 'create categories'
 fruits = Category.create!(
   name: 'Fruits',
-  remote_photo_url: "https://images.unsplash.com/photo-1521732813055-61b789f61463?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+  remote_photo_url: "https://res.cloudinary.com/df8xepdmu/image/upload/v1576134732/Screenshot_2019-12-12_at_3.10.51_PM_bdfycp.png"
 )
 
 
 dairy = Category.create!(
   name: 'Dairy',
-  remote_photo_url: "https://images.unsplash.com/photo-1528750997573-59b89d56f4f7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=866&q=80"
+  remote_photo_url: "https://res.cloudinary.com/df8xepdmu/image/upload/v1576134537/Screenshot_2019-12-12_at_3.05.44_PM_fscbou.png"
 )
 
 
 grains = Category.create!(
   name: 'Grains',
-  remote_photo_url: "https://images.unsplash.com/photo-1542990253-a781e04c0082?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=984&q=80"
-)
-
-
-poultry = Category.create!(
-  name: 'Poultry',
-  remote_photo_url: "https://images.unsplash.com/photo-1529856426070-e610ede5a2fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+  remote_photo_url: "https://res.cloudinary.com/df8xepdmu/image/upload/v1576134617/Screenshot_2019-12-12_at_3.03.38_PM_hsaqly.png"
 )
 
 
 vegetables = Category.create!(
   name: 'Vegetables',
-  remote_photo_url: "https://images.unsplash.com/photo-1538159021332-8f28e9eca2fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+  remote_photo_url: "https://res.cloudinary.com/df8xepdmu/image/upload/v1576135447/Screenshot_2019-12-12_at_3.22.14_PM_kfcs7t.png"
+)
+
+
+snacks = Category.create!(
+  name: 'Snacks',
+  remote_photo_url: "https://res.cloudinary.com/df8xepdmu/image/upload/c_thumb,w_200,g_face/v1576119575/snack2_ch6ycl.jpg"
 )
 
 canned_foods = Category.create!(
   name: 'Canned Foods',
-  remote_photo_url: "https://images.unsplash.com/photo-1534483509719-3feaee7c30da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+  remote_photo_url: "https://res.cloudinary.com/df8xepdmu/image/upload/v1576119343/Canned-Food-Drive_canned_food_drive_16x9_abc9a6cb-e610-49e3-9252-c25be5a1b34b_tjnljh.jpg"
 )
 
-puts "create donor profile for first user"
+puts "create donor profile"
+
 Donor.create(
-  full_name: 'Mandy Tan',
+  full_name: 'Charmaine',
   contact_number: '91234567',
-  user: User.first
+  user: User.find_by(email:'charmaine@gmail.com')
 )
 
 Donor.create(
-  full_name: 'User 4',
+  full_name: 'Mike',
+  contact_number: '91234577',
+  user: User.find_by(email:'mike@gmail.com')
+)
+
+Donor.create(
+  full_name: 'Mandy',
+  contact_number: '82346278',
+  user: User.find_by(email:'mandy123@gmail.com')
+)
+
+Donor.create(
+  full_name: 'Anna',
+  contact_number: '82346678',
+  user: User.find_by(email:'anna123@gmail.com')
+)
+
+Donor.create(
+  full_name: 'George',
   contact_number: '82345674',
-  user: User.last
+  user: User.find_by(email:'george123@gmail.com')
 )
 
 puts 'create foods'
 Food.create!(
-  name: "Ripe Banana (4 large ones)",
-  remote_photo_url: "https://images.unsplash.com/photo-1571141380069-521a19e0576c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-  description: "Will be good for baking!",
-  purchase_date: Date.new(2018,8,18),
+  name: "A packet of tomatoes",
+  remote_photo_url: "https://res.cloudinary.com/df8xepdmu/image/upload/v1576120940/Screenshot_2019-12-12_at_11.18.30_AM_jumpr0.png",
+  description: "Unused pack of tomatoes",
+  purchase_date: Date.new(2018,12,9),
   location: "360, #08-02 Orchard Rd, International Building, 238869",
   latitude: 1.2979,
   longitude: 103.8314,
-  donor: Donor.first,
+  donor: Donor.find_by(full_name: 'Mike'),
+  categories: [vegetables]
+)
+
+
+Food.create!(
+  name: "A carton of eggs",
+  remote_photo_url: "https://res.cloudinary.com/df8xepdmu/image/upload/v1576129879/Screenshot_2019-12-12_at_1.49.57_PM_gefbzw.png",
+  description: "Baked too much cake. Extra eggs to give away.",
+  purchase_date: Date.new(2018,12,4),
+  location: "14 Scotts Rd, Singapore 228213",
+  latitude: 1.3040,
+  longitude: 103.8320,
+  donor: Donor.find_by(full_name: 'Mandy'),
+  categories: [dairy]
+)
+
+Food.create!(
+  name: "Premium brown pears",
+  remote_photo_url: "https://res.cloudinary.com/df8xepdmu/image/upload/v1576135069/pear_etrhmn.jpg",
+  description: "3 brown pears bought from NTUC.",
+  purchase_date: Date.new(2018,12,1),
+  location: "313 Orchard Rd, Singapore 238895",
+  latitude: 1.301040,
+  longitude: 103.838379,
+  donor: Donor.find_by(full_name: 'Charmaine'),
   categories: [fruits]
 )
 
 
 Food.create!(
-  name: "Carton of eggs",
-  remote_photo_url: "https://images.unsplash.com/photo-1556910116-e220f7127371?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-  description: "Baked too much cake. Extra eggs to give away.",
-  purchase_date: Date.new(2018,8,21),
-  location: "14 Scotts Rd, Singapore 228213",
-  latitude: 1.3040,
-  longitude: 103.8320,
-  donor: Donor.first,
-  categories: [dairy]
-)
-
-
-Food.create!(
-  name: "Heinz Tomato Ketchup",
-  remote_photo_url: "https://images.unsplash.com/flagged/photo-1569231290508-8105e6e9620c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-  description: "Free gift, but I don't like ketchup.",
-  purchase_date: Date.new(2018,8,30),
+  name: "A box of 12 Granola Bars",
+  remote_photo_url: "https://res.cloudinary.com/df8xepdmu/image/upload/v1576121608/granola_bars_yilxub.jpg",
+  description: "A box of 12 Granola Bars received a week ago.",
+  purchase_date: Date.new(2018,12,6),
   location: "313 Orchard Rd, Singapore 238895",
   latitude: 1.301040,
   longitude: 103.838379,
-  donor: Donor.last,
+  donor: Donor.find_by(full_name: 'Anna'),
+  categories: [snacks]
+)
+
+Food.create!(
+  name: "A can of Campbell's soup",
+  remote_photo_url: "https://res.cloudinary.com/df8xepdmu/image/upload/v1576134009/Screenshot_2019-12-12_at_2.59.29_PM_ku3fh4.png",
+  description: "Giving away Campbell's Condensed Soup - Cream of Mushroom.",
+  purchase_date: Date.new(2018,12,5),
+  location: "313 Orchard Rd, Singapore 238895",
+  latitude: 1.301040,
+  longitude: 103.838379,
+  donor: Donor.find_by(full_name: 'Mike'),
   categories: [canned_foods]
 )
 
 
 Food.create!(
-  name: "A bunch of 6 apples",
-  remote_photo_url: "https://images.unsplash.com/flagged/photo-1569231290508-8105e6e9620c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-  description: "6 apples from NTUC",
-  purchase_date: Date.new(2018,8,15),
-  location: "313 Orchard Rd, Singapore 238895",
-  latitude: 1.301040,
-  longitude: 103.838379,
-  donor: Donor.last,
-  categories: [fruits]
-)
-
-
-Food.create!(
-  name: "A bunch of 4 oranges",
-  remote_photo_url: "https://images.unsplash.com/flagged/photo-1569231290508-8105e6e9620c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-  description: "4 oranges from NTUC",
-  purchase_date: Date.new(2018,8,15),
-  location: "313 Orchard Rd, Singapore 238895",
-  latitude: 1.301040,
-  longitude: 103.838379,
-  donor: Donor.first,
-  categories: [fruits]
-)
-
-
-Food.create!(
   name: "A carton of milk",
-  remote_photo_url: "https://images.unsplash.com/photo-1556910116-e220f7127371?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-  description: "A carton of milk.",
-  purchase_date: Date.new(2018,8,21),
+  remote_photo_url: "https://res.cloudinary.com/df8xepdmu/image/upload/v1576136651/milk_dawtsi.jpg",
+  description: "My kids don't drink this milk.",
+  purchase_date: Date.new(2018,12,12),
   location: "14 Scotts Rd, Singapore 228213",
   latitude: 1.3040,
   longitude: 103.8320,
-  donor: Donor.last,
+  donor: Donor.find_by(full_name: 'Anna'),
   categories: [dairy]
 )
 
 
 Food.create!(
-  name: "A tub of yogurt",
-  remote_photo_url: "https://images.unsplash.com/photo-1556910116-e220f7127371?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-  description: "A tub of yogurt.",
+  name: "A tub of vanilla yogurt",
+  remote_photo_url: "https://res.cloudinary.com/df8xepdmu/image/upload/v1576121978/Screenshot_2019-12-12_at_11.38.34_AM_cv9rme.png",
+  description: "A tub of vanilla yogurt unopened.",
   purchase_date: Date.new(2018,8,21),
   location: "14 Scotts Rd, Singapore 228213",
   latitude: 1.3040,
   longitude: 103.8320,
-  donor: Donor.first,
+  donor: Donor.find_by(full_name: 'George'),
   categories: [dairy]
 )
 
 puts "create donation"
 Donation.create!(
-  user: User.find_by(username: 'user3'),
+  user: User.find_by(username: 'Anna'),
   food: Food.first
   )
 
 Donation.create!(
-  user: User.find_by(username: 'user2'),
+  user: User.find_by(username: 'George'),
   food: Food.last
   )
